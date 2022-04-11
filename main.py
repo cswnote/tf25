a4ed5d4e0e1defc0cd801a696fc1b68497366cdd
cswnote <cswnote@gmail.com>
2022-04-12 07:59:49 +0900

    second commit

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import tensorflow as tf
-from tensorflow.keras.layers import InputLayer, Dense
-from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import InputLayer, Activation, Dense, Flatten
 from tensorflow.keras.utils import plot_model
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras import Sequential, Model
 import numpy as np
 
 x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
 y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 
 model = Sequential([InputLayer(input_shape=x_data.shape[1:], name='input'),
-                    Dense(2, activation='sigmoid', name='dense_0'),
-                    Dense(1, activation='sigmoid', name='dense_1')])
-model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1), metrics=['accuracy'])
+                    Dense(10, activation='sigmoid', name='dense_0'),
+                    Dense(10, activation='sigmoid', name='dense_1'),
+                    Dense(10, activation='sigmoid', name='dense_2'),
+                    Dense(10, activation='sigmoid', name='dense_3'),
+                    Dense(1, activation='sigmoid', name='output')])
+model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.1), metrics=['accuracy'])
 model.summary()
-plot_model(model)
+plot_model(model, to_file='main.png')
 
-# history = model.fit(x_data, y_data, epochs=10000)
-#
-# predictions = model.predict(x_data)
-# print('Prediction: \n', predictions)
-#
-# score = model.evaluate(x_data, y_data)
-# print('Loss: ', score[0])
-# print('Accuracy: ', score[1])
+history = model.fit(x_data, y_data, epochs=5000)
+
+prediction = model.predict(x_data)
+print('Prediction: \n', prediction)
+
+score = model.evaluate(x_data, y_data)
+print('Loss: ', score[0])
+print('Accuracy: ', score[1])
--- temp.py
+++ temp.py
@@ -1,21 +1,29 @@
+# Lab 9 XOR
+# 9-3 deep and wide
 import tensorflow as tf
+from tensorflow.keras.utils import plot_model
 import numpy as np
 
 x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
 y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 
 tf.model = tf.keras.Sequential()
-tf.model.add(tf.keras.layers.Dense(units=2, input_dim=2))
-tf.model.add(tf.keras.layers.Activation('sigmoid'))
-tf.model.add(tf.keras.layers.Dense(units=1, input_dim=2))
-tf.model.add(tf.keras.layers.Activation('sigmoid'))
-tf.model.compile(loss='binary_crossentropy', optimizer=tf.optimizers.SGD(lr=0.1),  metrics=['accuracy'])
-tf.model.summary()
+tf.model.add(tf.keras.layers.Dense(units=10, input_dim=2, activation='sigmoid'))
+tf.model.add(tf.keras.layers.Dense(units=10, activation='sigmoid'))
+tf.model.add(tf.keras.layers.Dense(units=10, activation='sigmoid'))
+tf.model.add(tf.keras.layers.Dense(units=10, activation='sigmoid'))
+tf.model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-# history = tf.model.fit(x_data, y_data, epochs=10000)
-#
-# predictions = tf.model.predict(x_data)
-# print('Prediction: \n', predictions)
-#
-# score = tf.model.evaluate(x_data, y_data)
-# print('Accuracy: ', score[1])
+# SGD not working very well due to vanishing gradient problem, switched to Adam for now
+# or you may use activation='relu', study chapter 10 to know more on vanishing gradient problem.
+tf.model.compile(loss='binary_crossentropy', optimizer=tf.optimizers.Adam(lr=0.1), metrics=['accuracy'])
+tf.model.summary()
+plot_model(tf.model, to_file='temp.png')
+
+history = tf.model.fit(x_data, y_data, epochs=5000)
+
+predictions = tf.model.predict(x_data)
+print('Prediction: \n', predictions)
+
+score = tf.model.evaluate(x_data, y_data)
+print('Accuracy: ', score[1])
