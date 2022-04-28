5b036a20b6921d64667db23d32e5d4669502ec62
cswnote <cswnote@gmail.com>
2022-04-29 08:24:58 +0900

    freq commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical, plot_model
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import InputLayer, Input, Dense, Flatten, LSTMCell, LSTM, Dropout
+from tensorflow.keras.layers import InputLayer, Input, Dense, Flatten, LSTMCell, LSTM, Dropout, Conv2D, MaxPool2D, Conv3D, MaxPool3D
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.datasets import cifar10
 
@@ -9,17 +9,26 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
+cnn = 1
+
 tf.random.set_seed(111)
 
 (x_train_full, y_train_full), (x_test, y_test) = cifar10.load_data()
 print(x_train_full.shape, x_test.shape)
 print(y_train_full.shape, y_test.shape)
 
+if cnn == 1:
+    x_train_full = x_train_full.reshape(-1, 32, 32, 3, 1)
+    x_test = x_test.reshape(-1, 32, 32, 3, 1)
+    # x_train_full = tf.expand_dims(x_train_full.shape[:, -1], 1) # 방법을 찾아보자
+
+    print(x_train_full.shape)
+    print(x_test.shape)
+
 x_train, x_val, y_train, y_val = train_test_split(x_train_full, y_train_full, test_size=0.3, random_state=111)
 
-num_sample = 25
-random_idxs = np.random.randint(x_train.shape[0], size=num_sample)
-
+# num_sample = 25
+# random_idxs = np.random.randint(x_train.shape[0], size=num_sample)
 # plt.figure(figsize=(14, 25))
 # for i, idx in enumerate(random_idxs):
 #     img = x_train[idx, :]
@@ -41,17 +50,26 @@ x_test = x_test / 255.
 # y_val = to_categorical(y_val)
 # y_test = to_categorical(y_test)
 
-model = Sequential([InputLayer(input_shape=x_train.shape[1:], name='input'),
-                    Flatten(input_shape=x_train.shape[1:], name='flatten'),
-                    Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
-                    Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
-                    Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
-                    Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
-                    Dense(10, kernel_initializer='glorot_normal', activation='softmax', name='output')])
+if cnn == 1:
+    model = Sequential([InputLayer(input_shape=x_train.shape[1:], name='input'),
+                        Conv3D(filters=16, kernel_size=(3, 3, 3), input_shape=x_train.shape[1:], activation='relu', padding='SAME'),
+                        MaxPool3D(pool_size=(1, 1, 1), padding='SAME'),
+                        Flatten(),
+                        Dense(10, kernel_initializer='glorot_normal', activation='softmax')
+                        ])
+else:
+    model = Sequential([InputLayer(input_shape=x_train.shape[1:], name='input'),
+                        Flatten(input_shape=x_train.shape[1:], name='flatten'),
+                        Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
+                        Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
+                        Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
+                        Dense(1000, kernel_initializer='glorot_normal', activation='relu'),
+                        Dense(10, kernel_initializer='glorot_normal', activation='softmax', name='output')
+                        ])
+
 model.summary()
 plot_model(model, to_file='main.jpg', show_shapes=True)
-model.compile(loss='sparse_categorical_crossentropy', optimizer=SGD(learning_rate=0.01), metrics=['accuracy'])
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
 
 history = model.fit(x_train, y_train, epochs=100, validation_data=(x_val, y_val))
 record = history.history
