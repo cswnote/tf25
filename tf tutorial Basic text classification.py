911f19819d427c4f8e2c67b5229efe339933e79b
cswnote <103407371+cswnote@users.noreply.github.com>
2022-06-05 11:11:13 +0900

    Update tf tutorial Basic text classification.py

--- tf tutorial Basic text classification.py
+++ tf tutorial Basic text classification.py
@@ -25,6 +25,79 @@ os.listdir(train_dir)
 
 print(train_dir)
 
-# sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
-# with open(sample_file) as f:
-#     print(f.read())
+sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
+with open(sample_file) as f:
+    print(f.read())
+
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+for text_batch, label_batch in raw_train_ds.take(1):
+  for i in range(3):
+    print("Review", text_batch.numpy()[i])
+    print("Label", label_batch.numpy()[i])
+
+print("Label 0 corresponds to", raw_train_ds.class_names[0])
+print("Label 1 corresponds to", raw_train_ds.class_names[1])
+print(raw_train_ds.element_spec[0])
+print(raw_train_ds.element_spec[1])
+
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
+
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batch_size)
+
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
+
+sample_file = os.path.join(train_dir, 'neg/3_4.txt')
+print(tf.strings.lower(sample_file))
+print(tf.strings.lower(sample_file).numpy())
+with open(sample_file) as f:
+    sample_file = f.read()
+    print(sample_file)
+
+lowercase = tf.strings.lower(sample_file)
+print(lowercase.numpy())
+
+stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+print(stripped_html.numpy())
+
+stripped_html2 = tf.strings.regex_replace(stripped_html, '[%s]' % re.escape(string.punctuation), '')
+print(stripped_html2.numpy())
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = tf.keras.layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_test(text, label):
+  text = tf.expand.dims(text, -1)
+  return vectorize_layer(text), label
