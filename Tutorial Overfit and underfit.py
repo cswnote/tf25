1a236f9df78c53f1c99ac6cc5cd2eaca4d977d36
cswnote <cswnote@gmail.com>
2022-06-28 11:35:25 +0900

    from desktop

--- Tutorial Overfit and underfit.py
+++ Tutorial Overfit and underfit.py
@@ -27,4 +27,82 @@ gz = tf.keras.utils.get_file('HIGGS.csv.gz', 'http://mlphysics.ics.uci.edu/data/
 FEATURES = 28
 ds = tf.data.experimental.CsvDataset(gz, [float(),]*(FEATURES+1), compression_type='GZIP')
 
+
+def pack_row(*row):
+  label = row[0]
+  features = tf.stack(row[1:],1)
+  return features, label
+
+
+packed_ds = ds.batch(10000).map(pack_row).unbatch()
+
+for features,label in packed_ds.batch(1000).take(1):
+  print(features[0])
+  plt.hist(features.numpy().flatten(), bins = 101)
+
+
+N_VALIDATION = int(1e3)
+N_TRAIN = int(1e4)
+BUFFER_SIZE = int(1e4)
+BATCH_SIZE = 500
+STEPS_PER_EPOCH = N_TRAIN / BATCH_SIZE
+
+validate_ds = packed_ds.take(N_VALIDATION).cache()
+train_ds = packed_ds.take(N_TRAIN).cache()
+
+validate_ds = validate_ds.batch(BATCH_SIZE)
+train_ds = train_ds.shuffle(BUFFER_SIZE).repeat().batch(BATCH_SIZE)
+
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+    0.001,
+    decay_steps=STEPS_PER_EPOCH*1000,
+    decay_rate=1,
+    staircase=False)
+
+
+def get_optimizer():
+  print(lr_schedule)
+  return Adam(lr_schedule)
+
+
+step = np.linspace(0, 100000)
+lr = lr_schedule(step)
+plt.figure(figsize = (8, 6))
+plt.plot(step/STEPS_PER_EPOCH, lr)
+plt.ylim([0, max(plt.ylim())])
+plt.xlabel('Epoch')
+_ = plt.ylabel('Learning Rate')
+
+
+def get_callbacks(name):
+  return [
+          tfdocs.modeling.EpochDots(),
+          tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy', patience=200),
+          tf.keras.callbacks.TensorBoard(logdir/name)
+  ]
+
+
+def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
+  if optimizer is None:
+    optimizer = get_optimizer()
+  model.compile(optimizer=optimizer,
+                loss=BinaryCrossentropy(from_logits=True),
+                metrics=[BinaryCrossentropy(from_logits=True,
+                                            name='binary_crossentropy'),
+                         'accuracy']
+  )
+
+  model.summary()
+
+  history = model.fit(train_ds,
+                      steps_per_epoch = STEPS_PER_EPOCH,
+                      epochs=max_epochs,
+                      validation_data=validate_ds,
+                      callbacks=get_callbacks(name),
+                      verbose=0)
+  return history
+
+
+
+
 print('===============')
