3bbdbc04bb14a8e361f3a84d0c2a437b48b77103
cswnote <cswnote@gmail.com>
2022-07-13 08:18:56 +0900

    from desktop

--- tutorial Load CSV data.py
+++ tutorial Load CSV data.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Normalization, Dense
 
-# Make numpy values easier to read
+abalone_train = pd.read_csv("https://storage.googleapis.com/download.tensorflow.org/data/abalone_train.csv",
+                           names=['Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight',
+                                 'Viscera weight', 'Shell weight', 'Age'])
+
+abalone_features = abalone_train.copy()
+abalone_labels = abalone_features.pop('Age')
+
+abalone_features = np.array(abalone_features)
+print(abalone_features)
