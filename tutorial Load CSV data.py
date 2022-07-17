d083345921572dc9d703e991e05d1e33f456829c
cswnote <cswnote@gmail.com>
2022-07-18 07:58:22 +0900

    from desktop

--- tutorial Load CSV data.py
+++ tutorial Load CSV data.py
@@ -17,24 +17,10 @@ import pathlib
 font_csvs = sorted(str(p) for p in pathlib.Path('fonts').glob('*.csv'))
 font_csvs[:10]
 
-font_line = pathlib.Path(font_csvs[0]).read_text().splitlines()[1]
-print(font_line)
-
-num_font_features = len(font_line.split(','))
-font_column_types = [str(), str()] + [float()] * (num_font_features - 2)
-
-simple_font_ds = tf.data.experimental.CsvDataset(
-    font_csvs, record_defaults=font_column_types, header=True)
-
-font_files = tf.data.Dataset.list_files('fonts/*.csv')
-
-def make_font_csv_ds(path):
-    return tf.data.experimental.CsvDataset(
-        path, record_defaults=font_column_types, header=True)
-
-font_rows = font_files.interleave(make_font_csv_ds, cycle_length=3)
-
-temp = font_rows.take(1)
+BATCH_SIZE = 2048
+fonts_ds = tf.data.experimental.make_csv_dataset(
+    file_pattern = 'fonts/*.csv', batch_size=BATCH_SIZE,
+    num_epochs=1,num_parallel_reads=100)
 
 print('=================')
 print('=================')
