46c1aab9b0c54e299d8045e4286aa48d3ee00f0c
cswnote <cswnote@gmail.com>
2022-07-18 17:15:11 +0900

    from M1 Macbook Air

--- tutorial Load CSV data.py
+++ tutorial Load CSV data.py
@@ -1,26 +1,38 @@
-import pathlib
-
 import pandas as pd
 import numpy as np
 
-# Make numpy values easier to read.
+# make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
 
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Normalization
+
+print(tf.__version__)
 
 import os
-
 path = os.getcwd()
+abalone_train = pd.read_csv(
+    path + "/abalone/abalone_train.csv",
+    # "https://storage.googleapis.com/download.tensorflow.org/data/abalone_train.csv",
+    names=["Length", "Diameter", "Height", "Whole weight", "Shucked weight",
+           "Viscera weight", "Shell weight", "Age"])
 
-import pathlib
-font_csvs = sorted(str(p) for p in pathlib.Path('fonts').glob('*.csv'))
-font_csvs[:10]
+abalone_features = abalone_train.copy()
+abalone_labels = abalone_features.pop('Age')
 
-BATCH_SIZE = 2048
-fonts_ds = tf.data.experimental.make_csv_dataset(
-    file_pattern = 'fonts/*.csv', batch_size=BATCH_SIZE,
-    num_epochs=1,num_parallel_reads=100)
+# # Normalization. It is possible that normalize input type is 'dataframe'(is composed only numeric data)
+normalize = Normalization()
+normalize.adapt(abalone_features)
 
-print('=================')
-print('=================')
+norm_abalone_model = Sequential([normalize,
+                                 Dense(64),
+                                 Dense(1)])
+norm_abalone_model.compile(loss=keras.losses.MeanSquaredError(),
+                           optimizer=keras.optimizers.Adam())
+norm_abalone_model.fit(abalone_features, abalone_labels, epochs=10)
+
+print("=======================")
+print("=======================")
