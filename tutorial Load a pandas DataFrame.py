71cb602ac6e91af37d6427692ecf0821b63ea972
cswnote <cswnote@gmail.com>
2022-07-25 01:38:13 +0900

    from M1 Macbook Air

---
+++ tutorial Load a pandas DataFrame.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import Input, InputLayer, Dense, Flatten, Normalization
+
+SHUFFLE_BUFFER = 500
+BATCH_SIZE = 2
+
+csv_file = tf.keras.utils.get_file('heart.csv', 'https://storage.googleapis.com/download.tensorflow.org/data/heart.csv')
+
+df = pd.read_csv(csv_file)
+
+target = df.pop('target')
+
+numeric_feature_names = ['age', 'thalach', 'trestbps', 'chol', 'oldpeak']
+numeric_features = df[numeric_feature_names]
+
+normalizer = tf.keras.layers.Normalization(axis=-1)
+normalizer.adapt(numeric_features)
+
+def get_basic_model():
+    model = tf.keras.Sequential([
+                                 normalizer,
+                                 tf.keras.layers.Dense(10, activation='relu'),
+                                 tf.keras.layers.Dense(10, activation='relu'),
+                                 tf.keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    return model
+
+model = get_basic_model()
+model.fit(numeric_features, target, epochs=15, batch_size=BATCH_SIZE)
