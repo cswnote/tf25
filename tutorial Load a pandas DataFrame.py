e0dd5b9739a01e27c69ca46b94c068122db7964c
cswnote <cswnote@gmail.com>
2022-07-27 08:10:47 +0900

    from desktop

--- tutorial Load a pandas DataFrame.py
+++ tutorial Load a pandas DataFrame.py
@@ -1,46 +1,104 @@
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.layers import Input, InputLayer, Dense, Flatten, Normalization
+from tensorflow.keras import Sequential
+
+print(tf.__version__)
 
 SHUFFLE_BUFFER = 500
 BATCH_SIZE = 2
 
 csv_file = tf.keras.utils.get_file('heart.csv', 'https://storage.googleapis.com/download.tensorflow.org/data/heart.csv')
 
+
+def stack_dict(inputs, fun=tf.stack):
+    values = []
+    for key in sorted(inputs.keys()):
+        values.append(tf.cast(inputs[key], tf.float32))
+
+    return fun(values, axis=-1)
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        # Create all the internal layers in init.
+        super().__init__(self)
+
+        self.normalizer = Normalization(axis=-1)
+
+        self.seq = Sequential([
+                               self.normalizer,
+                               Dense(10, activation='relu'),
+                               Dense(10, activation='relu'),
+                               Dense(1)
+        ])
+
+    def adapt(self, inputs):
+        # Stack the inputs and `adapt` the normalization layer.
+        inputs = stack_dict(inputs)
+        self.normalizer.adapt(inputs)
+
+    def call(self, inputs):
+        # Stack the inputs
+        inputs = stack_dict(inputs)
+        # Run them through all layers.
+        result = self.seq(inputs)
+
+        return result
+
+
 df = pd.read_csv(csv_file)
+print('df')
+print(df.head())
+print(df.dtypes)
+print()
 
 target = df.pop('target')
 
-numeric_feature_names = ['age', 'thalach', 'trestbps', 'chol', 'oldpeak']
-numeric_features = df[numeric_feature_names]
+numeric_features_name = ['age', 'thalach', 'trestbps', 'chol', 'oldpeak']
+numeric_features = df[numeric_features_name]
+print('numeric_features')
+print(numeric_features)
+print()
+
+
+numeric_features_tf = tf.convert_to_tensor(numeric_features)
+print("apply 'tf.conver_to_tensor' to numeric_features")
+print(numeric_features_tf.numpy())
+print()
 
 normalizer = tf.keras.layers.Normalization(axis=-1)
 normalizer.adapt(numeric_features)
+print()
 
-def get_basic_model():
-    model = tf.keras.Sequential([
-                                 normalizer,
-                                 tf.keras.layers.Dense(10, activation='relu'),
-                                 tf.keras.layers.Dense(10, activation='relu'),
-                                 tf.keras.layers.Dense(1)
-    ])
+print('normalizer numeric_features')
+print(normalizer(numeric_features.iloc[:3]))
+print()
 
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    return model
-
-# model = get_basic_model()
-# model.fit(numeric_features, target, epochs=15, batch_size=BATCH_SIZE)
-
+print("numeric_dataset: apply 'Dataset.from_tensor_slices' on (numeric_features, target)")
 numeric_dataset = tf.data.Dataset.from_tensor_slices((numeric_features, target))
-
 for row in numeric_dataset.take(3):
     print(row)
+print()
 
-values = []
-for key in sorted(dict(numeric_features)):
-    values.append(tf.cast(dict(numeric_features)[key], tf.float32))
+print("numeric_dict_ds: apply 'Dataset.from_tenor_slices on (dict(numeric_features), target)")
+numeric_dict_ds = tf.data.Dataset.from_tensor_slices((dict(numeric_features), target))
+for row in numeric_dict_ds.take(3):
+    print(row)
+print()
 
-print("==========================")
+
+model2 = MyModel()
+model2.adapt(numeric_features)
+model2.compile(optimizer='adam',
+               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+               metrics=['accuracy'],
+               run_eagerly=True)
+model2.fit(numeric_features, target, epochs=5, batch_size=BATCH_SIZE)
+
+
+
+
+
+
+print("======================================")
