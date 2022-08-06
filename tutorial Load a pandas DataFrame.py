68aab27b5dd731cf576da74d52df482d404d8c09
cswnote <cswnote@gmail.com>
2022-08-06 15:43:09 +0900

    from M1 Macbook Air

--- tutorial Load a pandas DataFrame.py
+++ tutorial Load a pandas DataFrame.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.layers import Input, InputLayer, Dense, Flatten, Normalization
-from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Normalization
+from tensorflow.keras import Sequential, Input
 
 print(tf.__version__)
 
@@ -10,101 +10,34 @@ BATCH_SIZE = 2
 
 csv_file = tf.keras.utils.get_file('heart.csv', 'https://storage.googleapis.com/download.tensorflow.org/data/heart.csv')
 
-
-def stack_dict(inputs, fun=tf.stack):
-    values = []
-    for key in sorted(inputs.keys()):
-        values.append(tf.cast(inputs[key], tf.float32))
-
-    return fun(values, axis=-1)
-
-
-class MyModel(tf.keras.Model):
-    def __init__(self):
-        # Create all the internal layers in init.
-        super().__init__(self)
-
-        self.normalizer = Normalization(axis=-1)
-
-        self.seq = Sequential([
-                               self.normalizer,
-                               Dense(10, activation='relu'),
-                               Dense(10, activation='relu'),
-                               Dense(1)
-        ])
-
-    def adapt(self, inputs):
-        # Stack the inputs and `adapt` the normalization layer.
-        inputs = stack_dict(inputs)
-        self.normalizer.adapt(inputs)
-
-    def call(self, inputs):
-        # Stack the inputs
-        inputs = stack_dict(inputs)
-        # Run them through all layers.
-        result = self.seq(inputs)
-
-        return result
-
-
 df = pd.read_csv(csv_file)
-print('df')
-print(df.head())
-print(df.dtypes)
-print()
 
 target = df.pop('target')
 
-numeric_features_name = ['age', 'thalach', 'trestbps', 'chol', 'oldpeak']
-numeric_features = df[numeric_features_name]
-print('numeric_features')
-print(numeric_features)
-print()
+binary_feature_names = []
+categorical_feature_names = []
 
-print("numeric_features_tf is applied numeric_features to 'tf.conver_to_tensor'")
-numeric_features_tf = tf.convert_to_tensor(numeric_features, target)
-print(numeric_features_tf.numpy())
-print()
-
-normalizer = tf.keras.layers.Normalization(axis=-1)
-normalizer.adapt(numeric_features)
-print()
-
-print('normalizer numeric_features')
-print(normalizer(numeric_features.iloc[:3]))
-print()
-
-print("numeric_dataset is applied (numeric_features, target) to 'Dataset.from_tensor_slices'")
-numeric_dataset = tf.data.Dataset.from_tensor_slices((numeric_features, target))
-for row in numeric_dataset.take(3):
-    print(row)
-print()
-
-print("numeric_dict_ds: apply 'Dataset.from_tenor_slices on (dict(numeric_features), target)")
-numeric_dict_ds = tf.data.Dataset.from_tensor_slices((dict(numeric_features), target))
-for row in numeric_dict_ds.take(3):
-    print(row)
-print()
-
-binary_feature_names = ['sex', 'fbs', 'exang']
-categorical_feature_names = ['cp', 'restecg', 'slope', 'thal', 'ca']
+for name, values in df.items():
+    nunique = df[name].nunique()
+    print(f"{name:10s}: {nunique}", end='\t')
+    if nunique == 2:
+        print(f"recommand to Binary")
+        binary_feature_names.append(name)
+    elif nunique <= 10:
+        print(f"recommand to Categorical")
+        categorical_feature_names.append(name)
+    else:
+        print(f"recommand to numerical")
 
 inputs = {}
 for name, column in df.items():
     if type(column[0]) == str:
         dtype = tf.string
-    elif (name in binary_feature_names or
-          name in categorical_feature_names):
+    elif (name in categorical_feature_names or
+          name in binary_feature_names):
         dtype = tf.int64
     else:
         dtype = tf.float32
 
-    # # don't refet to column, but column data is organized into input layers.................. why
-    inputs[name] = tf.keras.layers.Input(shape=(), name=name, dtype=dtype)
+    inputs[name] = Input(shape=(), name=name, dtype=dtype)
 
-
-
-
-
-print("======================================")
-print("======================================")
