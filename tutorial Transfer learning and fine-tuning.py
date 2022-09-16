8d6759d13c32643e95d15f9327686c60c027ce03
cswnote <cswnote@gmail.com>
2022-09-16 12:54:39 +0900

    from desktop

---
+++ tutorial Transfer learning and fine-tuning.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+import platform
+
+if platform.platform()[:3].lower() == 'win':
+    cache_subdir = 'D:/workspace/Pycharm/tf25/dataset/'
+elif platform.platform()[:3].lower() == 'mac':
+    cache_subdir = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/tf25/cats_and_dogs/'
+
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip',
+                                      cache_subdir=cache_subdir,
+                                      origin=_URL, extract=True)
+
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
+                                                            shuffle=False,
+                                                            batch_size=BATCH_SIZE,
+                                                            image_size=IMG_SIZE)
+
+print('====================')
+print('====================')
